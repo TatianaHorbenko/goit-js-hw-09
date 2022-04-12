5cbc5bc52f8b819b80a7912dd6c71729a121178d
TatianaHorbenko <3435640@gmail.com>
2022-04-12 15:25:27 +0300

    5

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -64,9 +64,6 @@ startBtn.addEventListener("click", () => {
 
 });
 
-function addLeadingZero(num) {
-    return String(num).padStart(2, "0");
-}
 
 function convertMs(ms) {
     // Number of milliseconds per unit of time
